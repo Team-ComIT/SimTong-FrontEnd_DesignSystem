26bb13c4b814dae67df3cd7dd3ef791c94f1949f
김벽걸 <biob1121@naver.com>
2022-08-08 01:24:04 +0900

    feat(DesignSystem): Color 시스템 추가

---
+++ simtong/package/color/theme.ts
@@ -0,0 +1,29 @@
+export const theme = {
+    color: {
+        color01: '#FFEDEE',
+        color02: '#FFC9CB',
+        color03: '#FF999C',
+        subColorOne: '#ED666A',
+        mainColor: '#E84045',
+        color06: '#DC3035',
+        subColorTwo: '#C32328',
+        color08: '#971D20',
+        color09: '#4D0002',
+        gray01: '#FDFDFD',
+        gray02: '#EDEDED',
+        gray03: '#D3D3D3',
+        gray04: '#BDBDBD',
+        gray05: '#A7A7A7',
+        gray06: '#929292',
+        gray07: '#7C7C7C',
+        gray08: '#505050',
+        mainBlack: '#242424',
+        white: '#FFFFFF',
+        black: '#000000',
+        focusBlue: '#006EFF',
+        checkColor: '#04DF00',
+        errorColor: '#DC3035',
+    },
+} as const;
+
+export default theme;

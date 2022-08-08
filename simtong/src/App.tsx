ff2d2599082bb909b5ec8f10aff6417846353a1c
김벽걸 <biob1121@naver.com>
2022-08-08 20:14:48 +0900

    fix(Jiwon): 부적합 오류 해결 2

--- simtong/src/App.tsx
+++ simtong/src/App.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import Button from "./package/ui/Button";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Button buttonType="GrayText" content="가나다" />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

a80238f24117ccad8b2e6eec1eeb61cf6ca6982c
김벽걸 <biob1121@naver.com>
2022-08-08 17:43:09 +0900

    feat(Jiwon): Font 시스템 추가

--- simtong/src/App.tsx
+++ simtong/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Global } from "@emotion/react";
+import style from "./GlobalStyle";
 import Button from "./package/ui/Button";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Global styles={style} />
       <Button buttonType="GrayText" content="가나다" />
     </div>
   );

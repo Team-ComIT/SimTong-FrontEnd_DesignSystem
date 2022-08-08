90bf34c117341423ebc53ea8cd3e8d89153b3279
김벽걸 <biob1121@naver.com>
2022-08-08 11:28:46 +0900

    feat(Jiwon): 버튼 시스템 추가

--- simtong/src/App.tsx
+++ simtong/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Button from "./package/ui/Button";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button buttonType="GrayText" content="가나다" />
+    </div>
+  );
 }
 
 export default App;

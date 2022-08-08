d38afddf12c40efacd6a447085ca3ce56cf31031
김벽걸 <biob1121@naver.com>
2022-08-08 20:13:59 +0900

    fix(Jiwon):  부적합 오류 해결

--- simtong/src/App.tsx
+++ simtong/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Global } from "@emotion/react";
-import style from "./GlobalStyle";
 import Button from "./package/ui/Button";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Global styles={style} />
       <Button buttonType="GrayText" content="가나다" />
     </div>
   );
